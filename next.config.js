01651bec6d40d2054aa6bdf8296cded33ce4816e
Md Nur Mohammad <ridz@shothik.ai>
2023-08-15 21:06:21 +0600

    Update HOST API URL

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   env: {
     DEV_API: 'http://localhost:3000',
-    PRODUCTION_API: 'https://api-dev-minimal-v510.vercel.app',
+    PRODUCTION_API: 'http://localhost:3000',
   },
 };
