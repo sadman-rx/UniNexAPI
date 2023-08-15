5ea659570e8f81bea167a84569ba3f44f7eb13f1
Md Nur Mohammad <ridz@shothik.ai>
2023-08-15 21:24:39 +0600

    Update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function IndexPage() {
   const renderHead = (
     <Stack spacing={2} sx={{ textAlign: 'center' }}>
       <Typography variant="h5" component="h1" sx={{ fontWeight: 'fontWeightBold' }}>
-        The starting point for your next project v5.1.0
+        UniNex API
       </Typography>
 
       <Typography variant="body2">
@@ -41,252 +41,252 @@ export default function IndexPage() {
     </Stack>
   );
 
-  const renderBlog = (
-    <Stack spacing={1}>
-      <Typography variant="h6" sx={{ fontWeight: 'fontWeightBold' }}>
-        Blog
-      </Typography>
+  // const renderBlog = (
+  //   <Stack spacing={1}>
+  //     <Typography variant="h6" sx={{ fontWeight: 'fontWeightBold' }}>
+  //       Blog
+  //     </Typography>
 
-      <Block method="GET" description="Get all posts" path="/api/post/list" />
-      <Block
-        method="GET"
-        description="Get post details by title"
-        path={
-          <>
-            /api/post/details?title=<strong>{`{title}`}</strong>
-          </>
-        }
-      />
-      <Block
-        method="GET"
-        description="Get latest posts"
-        path={
-          <>
-            /api/post/latest?title=<strong>{`{title}`}</strong>
-          </>
-        }
-      />
-      <Block
-        method="GET"
-        description="Search post"
-        path={
-          <>
-            /api/post/search?query=<strong>{`{query}`}</strong>
-          </>
-        }
-      />
-    </Stack>
-  );
+  //     <Block method="GET" description="Get all posts" path="/api/post/list" />
+  //     <Block
+  //       method="GET"
+  //       description="Get post details by title"
+  //       path={
+  //         <>
+  //           /api/post/details?title=<strong>{`{title}`}</strong>
+  //         </>
+  //       }
+  //     />
+  //     <Block
+  //       method="GET"
+  //       description="Get latest posts"
+  //       path={
+  //         <>
+  //           /api/post/latest?title=<strong>{`{title}`}</strong>
+  //         </>
+  //       }
+  //     />
+  //     <Block
+  //       method="GET"
+  //       description="Search post"
+  //       path={
+  //         <>
+  //           /api/post/search?query=<strong>{`{query}`}</strong>
+  //         </>
+  //       }
+  //     />
+  //   </Stack>
+  // );
 
-  const renderCalendar = (
-    <Stack spacing={1}>
-      <Typography variant="h6" sx={{ fontWeight: 'fontWeightBold' }}>
-        Calendar
-      </Typography>
+  // const renderCalendar = (
+  //   <Stack spacing={1}>
+  //     <Typography variant="h6" sx={{ fontWeight: 'fontWeightBold' }}>
+  //       Calendar
+  //     </Typography>
 
-      <Block method="GET" description="Get all events" path="/api/calendar" />
-      <Block method="POST" description="Create new event" path="/api/calendar" />
-      <Block method="PUT" description="Update event" path="/api/calendar" />
-      <Block method="PATCH" description="Delete event" path="/api/calendar" />
-    </Stack>
-  );
+  //     <Block method="GET" description="Get all events" path="/api/calendar" />
+  //     <Block method="POST" description="Create new event" path="/api/calendar" />
+  //     <Block method="PUT" description="Update event" path="/api/calendar" />
+  //     <Block method="PATCH" description="Delete event" path="/api/calendar" />
+  //   </Stack>
+  // );
 
-  const renderChat = (
-    <Stack spacing={1}>
-      <Typography variant="h6" sx={{ fontWeight: 'fontWeightBold' }}>
-        Chat
-      </Typography>
+  // const renderChat = (
+  //   <Stack spacing={1}>
+  //     <Typography variant="h6" sx={{ fontWeight: 'fontWeightBold' }}>
+  //       Chat
+  //     </Typography>
 
-      <Block
-        method="GET"
-        description="Search contacts"
-        path={
-          <>
-            /api/chat?endpoint=<strong>contacts</strong>
-          </>
-        }
-      />
-      <Block
-        method="GET"
-        description="Get all conversations"
-        path={
-          <>
-            /api/chat?endpoint=<strong>conversations</strong>
-          </>
-        }
-      />
-      <Block
-        method="GET"
-        description="Get conversation details by ID"
-        path={
-          <>
-            /api/chat?conversationId=<strong>{`{conversationId}`}</strong>&endpoint=
-            <strong>conversation</strong>
-          </>
-        }
-      />
-      <Block
-        method="GET"
-        description="Mark conversation as seen when click"
-        path={
-          <>
-            /api/chat?conversationId=<strong>{`{conversationId}`}</strong>&endpoint=
-            <strong>mark-as-seen</strong>
-          </>
-        }
-      />
+  //     <Block
+  //       method="GET"
+  //       description="Search contacts"
+  //       path={
+  //         <>
+  //           /api/chat?endpoint=<strong>contacts</strong>
+  //         </>
+  //       }
+  //     />
+  //     <Block
+  //       method="GET"
+  //       description="Get all conversations"
+  //       path={
+  //         <>
+  //           /api/chat?endpoint=<strong>conversations</strong>
+  //         </>
+  //       }
+  //     />
+  //     <Block
+  //       method="GET"
+  //       description="Get conversation details by ID"
+  //       path={
+  //         <>
+  //           /api/chat?conversationId=<strong>{`{conversationId}`}</strong>&endpoint=
+  //           <strong>conversation</strong>
+  //         </>
+  //       }
+  //     />
+  //     <Block
+  //       method="GET"
+  //       description="Mark conversation as seen when click"
+  //       path={
+  //         <>
+  //           /api/chat?conversationId=<strong>{`{conversationId}`}</strong>&endpoint=
+  //           <strong>mark-as-seen</strong>
+  //         </>
+  //       }
+  //     />
 
-      <Block method="POST" description="Create new conversation" path={`${HOST_API}/api/chat`} />
-      <Block method="PUT" description="Update conversation" path={`${HOST_API}/api/chat`} />
-    </Stack>
-  );
+  //     <Block method="POST" description="Create new conversation" path={`${HOST_API}/api/chat`} />
+  //     <Block method="PUT" description="Update conversation" path={`${HOST_API}/api/chat`} />
+  //   </Stack>
+  // );
 
-  const renderKanban = (
-    <Stack spacing={1}>
-      <Typography variant="h6" sx={{ fontWeight: 'fontWeightBold' }}>
-        Kanban
-      </Typography>
+  // const renderKanban = (
+  //   <Stack spacing={1}>
+  //     <Typography variant="h6" sx={{ fontWeight: 'fontWeightBold' }}>
+  //       Kanban
+  //     </Typography>
 
-      <Block method="GET" path="/api/kanban" description="Get Board" />
-      <Block
-        method="POST"
-        description="Create column"
-        path={
-          <>
-            /api/kanban?endpoint=<strong>create-column</strong>
-          </>
-        }
-      />
-      <Block
-        method="POST"
-        description="Update column"
-        path={
-          <>
-            /api/kanban?endpoint=<strong>update-column</strong>
-          </>
-        }
-      />
-      <Block
-        method="POST"
-        description="Move column"
-        path={
-          <>
-            /api/kanban?endpoint=<strong>move-column</strong>
-          </>
-        }
-      />
-      <Block
-        method="POST"
-        description="Clear column"
-        path={
-          <>
-            /api/kanban?endpoint=<strong>clear-column</strong>
-          </>
-        }
-      />
-      <Block
-        method="POST"
-        description="Delete column"
-        path={
-          <>
-            /api/kanban?endpoint=<strong>delete-column</strong>
-          </>
-        }
-      />
-      <Block
-        method="POST"
-        description="Create task"
-        path={
-          <>
-            /api/kanban?endpoint=<strong>delete-task</strong>
-          </>
-        }
-      />
-      <Block
-        method="POST"
-        description="Update task"
-        path={
-          <>
-            /api/kanban?endpoint=<strong>update-task</strong>
-          </>
-        }
-      />
-      <Block
-        method="POST"
-        description="Move task"
-        path={
-          <>
-            /api/kanban?endpoint=<strong>move-task</strong>
-          </>
-        }
-      />
-      <Block
-        method="POST"
-        description="Delete task"
-        path={
-          <>
-            /api/kanban?endpoint=<strong>delete-task</strong>
-          </>
-        }
-      />
-    </Stack>
-  );
+  //     <Block method="GET" path="/api/kanban" description="Get Board" />
+  //     <Block
+  //       method="POST"
+  //       description="Create column"
+  //       path={
+  //         <>
+  //           /api/kanban?endpoint=<strong>create-column</strong>
+  //         </>
+  //       }
+  //     />
+  //     <Block
+  //       method="POST"
+  //       description="Update column"
+  //       path={
+  //         <>
+  //           /api/kanban?endpoint=<strong>update-column</strong>
+  //         </>
+  //       }
+  //     />
+  //     <Block
+  //       method="POST"
+  //       description="Move column"
+  //       path={
+  //         <>
+  //           /api/kanban?endpoint=<strong>move-column</strong>
+  //         </>
+  //       }
+  //     />
+  //     <Block
+  //       method="POST"
+  //       description="Clear column"
+  //       path={
+  //         <>
+  //           /api/kanban?endpoint=<strong>clear-column</strong>
+  //         </>
+  //       }
+  //     />
+  //     <Block
+  //       method="POST"
+  //       description="Delete column"
+  //       path={
+  //         <>
+  //           /api/kanban?endpoint=<strong>delete-column</strong>
+  //         </>
+  //       }
+  //     />
+  //     <Block
+  //       method="POST"
+  //       description="Create task"
+  //       path={
+  //         <>
+  //           /api/kanban?endpoint=<strong>delete-task</strong>
+  //         </>
+  //       }
+  //     />
+  //     <Block
+  //       method="POST"
+  //       description="Update task"
+  //       path={
+  //         <>
+  //           /api/kanban?endpoint=<strong>update-task</strong>
+  //         </>
+  //       }
+  //     />
+  //     <Block
+  //       method="POST"
+  //       description="Move task"
+  //       path={
+  //         <>
+  //           /api/kanban?endpoint=<strong>move-task</strong>
+  //         </>
+  //       }
+  //     />
+  //     <Block
+  //       method="POST"
+  //       description="Delete task"
+  //       path={
+  //         <>
+  //           /api/kanban?endpoint=<strong>delete-task</strong>
+  //         </>
+  //       }
+  //     />
+  //   </Stack>
+  // );
 
-  const renderMail = (
-    <Stack spacing={1}>
-      <Typography variant="h6" sx={{ fontWeight: 'fontWeightBold' }}>
-        Mail
-      </Typography>
+  // const renderMail = (
+  //   <Stack spacing={1}>
+  //     <Typography variant="h6" sx={{ fontWeight: 'fontWeightBold' }}>
+  //       Mail
+  //     </Typography>
 
-      <Block method="GET" description="Get all labels" path="/api/mail/labels" />
-      <Block
-        method="GET"
-        description="Get mails by labelId"
-        path={
-          <>
-            /api/mail/list?labelId=<strong>{`{labelId}`}</strong>
-          </>
-        }
-      />
-      <Block
-        method="GET"
-        description="Get mail details by ID"
-        path={
-          <>
-            /api/mail/details?mailId=<strong>{`{mailId}`}</strong>
-          </>
-        }
-      />
-    </Stack>
-  );
+  //     <Block method="GET" description="Get all labels" path="/api/mail/labels" />
+  //     <Block
+  //       method="GET"
+  //       description="Get mails by labelId"
+  //       path={
+  //         <>
+  //           /api/mail/list?labelId=<strong>{`{labelId}`}</strong>
+  //         </>
+  //       }
+  //     />
+  //     <Block
+  //       method="GET"
+  //       description="Get mail details by ID"
+  //       path={
+  //         <>
+  //           /api/mail/details?mailId=<strong>{`{mailId}`}</strong>
+  //         </>
+  //       }
+  //     />
+  //   </Stack>
+  // );
 
-  const renderProduct = (
-    <Stack spacing={1}>
-      <Typography variant="h6" sx={{ fontWeight: 'fontWeightBold' }}>
-        Product
-      </Typography>
+  // const renderProduct = (
+  //   <Stack spacing={1}>
+  //     <Typography variant="h6" sx={{ fontWeight: 'fontWeightBold' }}>
+  //       Product
+  //     </Typography>
 
-      <Block method="GET" description="Get all products" path="/api/product/list" />
-      <Block
-        method="GET"
-        description="Get product details by ID"
-        path={
-          <>
-            /api/product/details?productId=<strong>{`{productId}`}</strong>
-          </>
-        }
-      />
-      <Block
-        method="GET"
-        description="Search product"
-        path={
-          <>
-            /api/product/search?query=<strong>{`{query}`}</strong>
-          </>
-        }
-      />
-    </Stack>
-  );
+  //     <Block method="GET" description="Get all products" path="/api/product/list" />
+  //     <Block
+  //       method="GET"
+  //       description="Get product details by ID"
+  //       path={
+  //         <>
+  //           /api/product/details?productId=<strong>{`{productId}`}</strong>
+  //         </>
+  //       }
+  //     />
+  //     <Block
+  //       method="GET"
+  //       description="Search product"
+  //       path={
+  //         <>
+  //           /api/product/search?query=<strong>{`{query}`}</strong>
+  //         </>
+  //       }
+  //     />
+  //   </Stack>
+  // );
 
   return (
     <Container
@@ -307,7 +307,7 @@ export default function IndexPage() {
 
         {renderAuth}
 
-        {renderBlog}
+        {/* {renderBlog}
 
         {renderCalendar}
 
@@ -317,7 +317,7 @@ export default function IndexPage() {
 
         {renderMail}
 
-        {renderProduct}
+        {renderProduct} */}
       </Stack>
     </Container>
   );
